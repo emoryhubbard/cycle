efc8af06e29fb77ecfa4557004363976775d56e0
KillerK1998 <kaler.sappington@gmail.com>
2022-11-18 17:04:59 -0700

    changes

--- control_actors_action.py
+++ control_actors_action.py
@@ -10,7 +10,7 @@ class ControlActorsAction(Action):
 
     def execute(self, cast):
         p1_direction = None #p1 hasn't pressed key
-        
+        p2_direction = None#p2 hasn't pressed key.
         if self._input_service.is_key_down('a'):
             p1_direction = Point(-constants.CELL_SIZE, 0)
 
@@ -27,3 +27,19 @@ class ControlActorsAction(Action):
         player1 = cast.get_first_actor("players")
         if p1_direction != None:
             player1.turn_bike(p1_direction)
+
+        if self._input_service.is_key_down('i'):
+            p2_direction = Point(-constants.CELL_SIZE, 0)
+
+        if self._input_service.is_key_down('j'):
+            p2_direction = Point(constants.CELL_SIZE, 0)
+
+        if self._input_service.is_key_down('k'):
+            p2_direction = Point(0, -constants.CELL_SIZE)
+
+        if self._input_service.is_key_down('l'):
+            p2_direction = Point(0, constants.CELL_SIZE)
+        #if p2_direction isn't None, p2 pressed the key
+        player2 = cast.get_first_actor("players")
+        if p2_direction != None:
+            player2.turn_bike(p2_direction)
